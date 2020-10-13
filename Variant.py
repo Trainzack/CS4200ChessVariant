7a166ae5e453630cc4ead0e2731eb9060a4fa6a7
Eli A Zupke <Trainzack@users.noreply.github.com>
2020-10-12 20:23:03 -0700

    Create Variant.py
    Added base Variant.py

---
+++ Variant.py
@@ -0,0 +1,19 @@
+# This class represents a chess variant.
+# This is what the genetic algroithm will develop
+
+
+
+class Variant:
+    """Represents a variant of the game chess."""
+
+
+
+    def getStartingFEN(self) -> str:
+        """Returns the starting FEN"""
+        # TODO
+        return ""
+
+    def getFairyStockfishINI(self) -> str:
+        """Returns the fairystockfish ini file that defines this variant"""
+        # TODO
+        return ""
